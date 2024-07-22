b0368955e918b00e252298193f67cc7f5cc7095c
siddhu <siddhumahesh333@gmail.com>
2024-07-22 15:57:11 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"   
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    exit 1 
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 
+else
+    echo "You are root user"
+fi
+
+dnf module disable mysql -y
+cp mysql.repo /etc/yum.repos.d/
+dnf install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
