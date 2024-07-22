083c5c56371f9c42513b749000383225877dfa7f
siddhu <siddhumahesh333@gmail.com>
2024-07-22 12:54:17 +0530

    s

--- ṃongodb.sh
+++ ṃongodb.sh
@@ -25,7 +25,7 @@ else
     echo "You are root user"
 fi
 
-cp -p mongo.repo >> /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongo.repo >> /etc/yum.repos.d/ &>> $LOGFILE
 VALIDATE $? "Copied MongoDB Repo"
 
 dnf install mongodb-org -y 
