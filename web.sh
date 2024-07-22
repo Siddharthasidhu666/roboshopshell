36fb4c21ef11304deb4e2c8e0bc52de4a2941797
siddhu <siddhumahesh333@gmail.com>
2024-07-22 13:24:04 +0530

    web

---
+++ web.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"   
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    exit 1 
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 
+else
+    echo "You are root user"
+fi
+
+dnf install nginx -y
+VALIDATE $? "insatlling nginx"
+
+systemctl enable nginx
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "removing files"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+VALIDATE $? "Download web file"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/web.zip
+VALIDATE $? "unzipping files"
+
+cp /root/roboshopshell/roboshop.conf /etc/nginx/default.d/
+VALIDATE $? "copying roboshop conf"
+
+sed -i "7s/localhost/54.87.110.249/" roboshop.conf
+VALIDATE $? "configured"
+
+
+systemctl restart nginx 
+VALIDATE $? "restarting nginx"
