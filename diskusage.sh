1b614d7bc52589d1e91f39a87ef9b53fa1e01328
siddhu <siddhumahesh333@gmail.com>
2024-07-27 11:24:58 +0530

    diskusaeeer

--- diskusage.sh
+++ diskusage.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 
 disk_usage=$(df -hT|grep "xfs")
 threshold=90
-message=[]
+message=""
 while IFS= read -r line; do
     usage=$(echo $line | awk -F ' '{print $6}"|cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
