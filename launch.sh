c672830f24962700c9cc24a09e9b3acbd29c3189
siddhu <siddhumahesh333@gmail.com>
2024-07-28 11:08:52 +0530

    launchhhhh

--- launch.sh
+++ launch.sh
@@ -8,6 +8,6 @@ do
     --image-id ami-0b4f379183e5706b9 \
     --instance-type $type \
     --security-group-ids sg-0ea30ddacc888b57c \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$item}]' \
+    --tag-specifications ResourceType=instance,Tags=[{Key=Name,Value=$item}] \
 
 done
