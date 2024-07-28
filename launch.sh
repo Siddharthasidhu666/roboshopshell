1390e3601b577ac19ed75626b387f75e8e7e1504
siddhu <siddhumahesh333@gmail.com>
2024-07-28 10:54:23 +0530

    launch

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+launch=(web app redis rabbitmq )
+type=t2.micro
+
+for item in $launch
+do
+aws ec2 run-instances \
+--image-id ami-0b4f379183e5706b9 \
+--instance-type $type \
+--security-group-ids sg-0ea30ddacc888b57c \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$launch}]' \
+done
