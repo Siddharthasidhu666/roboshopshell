7e21cfbd8263ad3226970c97287fa0af6ff058f7
siddhu <siddhumahesh333@gmail.com>
2024-07-28 10:56:50 +0530

    launchh

--- launch.sh
+++ launch.sh
@@ -8,5 +8,6 @@ aws ec2 run-instances \
 --image-id ami-0b4f379183e5706b9 \
 --instance-type $type \
 --security-group-ids sg-0ea30ddacc888b57c \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$launch}]' \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$item}]' \
+
 done
